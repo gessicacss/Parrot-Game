2e558a8aecd2ef416c51fe48f2ca77ce621434ca
Géssica Santos <santos.gessicacarolina@gmail.com>
2023-01-13 14:28:07 -0300

    adicionando função que vira as cartas (por enquanto não pode virar mais que duas)

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,3 @@
-const newDeck = [];
 const images = [
     "/assets/img/bobrossparrot.gif",
     "/assets/img/explodyparrot.gif",
@@ -9,52 +8,73 @@ const images = [
     "/assets/img/unicornparrot.gif"
 ];
 let cardHtml = document.querySelector('div');
+let firstCard,
+secondCard,
+img,
+imgTwo,
+stopTimer,
+revealedCards,
+quantityCards;
+let limitOfCards = 0;
+let countPlayed = 0;
+let timer = 0;
 
-let quantityCards = Number(prompt(`Quantas cartas quer jogar?`));
-
-while (quantityCards % 2 !== 0 || quantityCards > 14 || quantityCards < 3) {
-    alert (`Quantidade inválida. Por favor, escolha um número par entre 4 a 14`)
-    quantityCards = prompt(`Quantas cartas quer jogar?`);
-}
-
-//uma função que envie pro html as divs usando as imagens usadas no array
 function startingGame () {
-    images.sort(comparador); //eu senti que só com o newdeck.sort não 
-    //estava embaralhando, pode ser impressão, mas decidi deixar
 
-    for (let i = 0; i < quantityCards/2; i++) {
-        newDeck.push(images[i]);
-        newDeck.push(images[i]);
+    quantityCards = Number(prompt(`Quantas cartas quer jogar?`));
+
+    while (quantityCards % 2 !== 0 || quantityCards > 14 || quantityCards < 4) {
+        alert (`Quantidade inválida. Por favor, escolha um número par entre 4 a 14`)
+        quantityCards = prompt(`Quantas cartas quer jogar?`);
     }
 
-    newDeck.sort(comparador);
+    const newDeck = [];
+
+    for (let j = 0; j < quantityCards/2; j++) {
+        newDeck.push(images[j]);
+        newDeck.push(images[j]);
+    }
+
+    newDeck.sort(shuffle);
 
     for (let i = 0; i < quantityCards; i++) {
         cardHtml.innerHTML += `
-        <div class="card" onclick="revealCard(this)">
+        <div data-test="card" class="card" onclick="revealCard(this)" >
             <div class="front-face face">
-                <img src="./assets/img/back.png" alt "desenho de um papagaio">
+                <img data-test="face-down-image" src="./assets/img/back.png" alt "desenho de um papagaio">
             </div>
             <div class="back-face face">
-                <img src="${newDeck[i]}" alt="gif de um papagaio dançando"
+                <img data-test="face-up-image" src="${newDeck[i]}" alt="gif de um papagaio dançando"
             </div>
         </div>
         `
     }
+
+    limitOfCards = 2;
 }
 
-//criar uma função que quando clique na carta, ela vire e se não for igual ela desvira
-//criar uma função em que as imagens sejam lançadas duas iguais sempre
-//criar uma função que caso as cartas tenham a mesma imagem, ela permaneça virada e permita que continue virando as outroas
-//criar um contador
-//criar uma função que quando a quantidade de cartas colocada for a mesma que a quantidade de cartas viradas, ela crie um alerta dizendo que o jogo terminou
-// e que essa função diga quantos clicks tiveram, e em quanto tempo foi finalizado
-// criar uma função que pergunte ao terminar o jogo, se gostaria de recomeçar (para isso, faria sentido se o prompt de quantidade de jogos estivesse na função startingGame)
+function revealCard (card) {
+    if (document.querySelectorAll('.card.reveal').length >= limitOfCards){
+        return;
+    }
+    card.classList.add('reveal');
 
-
-
-function comparador() { 
+    let turnedCards = document.querySelectorAll('.card.reveal');
+    countPlayed++;
+    if((turnedCards.length)%2 !== 0 ) {
+        secondCard = card;
+    }
+    if ((turnedCards.length)%2 === 0) {
+        firstCard = card;
+        compareCards(card, secondCard);
+    }
+}
+function shuffle() { 
 	return Math.random() - 0.5; 
 }
 
 startingGame();
+//criar uma função que caso as cartas tenham a mesma imagem, ela permaneça virada e permita que continue virando as outroas
+//criar uma função que quando a quantidade de cartas colocada for a mesma que a quantidade de cartas viradas, ela crie um alerta dizendo que o jogo terminou
+// e que essa função diga quantos clicks tiveram, e em quanto tempo foi finalizado
+// criar uma função que pergunte ao terminar o jogo, se gostaria de recomeçar (para isso, faria sentido se o prompt de quantidade de jogos estivesse na função startingGame)
